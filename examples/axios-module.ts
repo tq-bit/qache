d2586aa414b8b5e7a678ebb40dd23b487c70fccd
q-bit <t.quante@outlook.com>
2022-05-22 16:52:42 +0200

    chore: add example for decoupled approach

--- examples/axios-module.ts
+++ examples/axios-module.ts
@@ -6,38 +6,23 @@ const cache = new Cache({
   lifetime: 1000 * 60 * 5,
 });
 
+// Create a custom axios instance
 const instance = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
-instance.interceptors.request.use((config) => {
-  const cached = cache.get(config.url || '');
-  if (cached) {
-    console.log('Receiving value from cache');
-    return {
-      ...config,
-      data: cached,
-      // The cancel token throws an error
-      cancelToken: new axios.CancelToken((cancel) => cancel()),
-    };
-  } else {
-    console.log('Executing http request');
-    return config;
-  }
-});
-
+// Add a single interceptor that sets cache data for a single request URL
 instance.interceptors.response.use((response) => {
   console.log('Setting cache data');
   cache.set(response.config.url || '', response.data);
   return response;
 });
 
+// Access the response and cached data manually
 (async function () {
-  try {
-    await instance.get('/todos/1');
-    await instance.get('/todos/1');
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await instance.get('/todos/1');
+  const cachedData = await cache.get('/todos/1');
+  console.log(response);
+  console.log(cachedData);
   process.exit(0);
 })();
