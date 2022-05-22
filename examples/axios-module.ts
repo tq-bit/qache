1e35ed576acccda1c0798497531423b31359bfb5
q-bit <t.quante@outlook.com>
2022-05-22 16:55:24 +0200

    chore: clean up examples and add types

--- examples/axios-module.ts
+++ examples/axios-module.ts
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import Cache from '../dist';
-const cache = new Cache({
+
+interface Post {
+  userId: string;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const cache = new Cache<Post>({
   cacheKey: 'posts',
   entryKey: 'id',
   lifetime: 1000 * 60 * 5,
@@ -14,15 +22,15 @@ const instance = axios.create({
 // Add a single interceptor that sets cache data for a single request URL
 instance.interceptors.response.use((response) => {
   console.log('Setting cache data');
-  cache.set(response.config.url || '', response.data);
+  cache.set(response.config.url || '', response.data as Post | Post[]);
   return response;
 });
 
 // Access the response and cached data manually
 (async function () {
-  const response = await instance.get('/todos/1');
-  const cachedData = await cache.get('/todos/1');
-  console.log(response);
+  const response = await instance.get('/posts/1');
+  const cachedData = cache.get('/posts/1');
+  console.log(response.data);
   console.log(cachedData);
   process.exit(0);
 })();
